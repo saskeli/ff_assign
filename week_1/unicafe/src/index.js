67b52986ea324fe204e5d09c09e5c057509c3f81
Dönges Saska I <saska@hpf-20.cs.helsinki.fi>
2019-05-20 17:37:56 +0300

    unicafe part 2

--- week_1/unicafe/src/index.js
+++ week_1/unicafe/src/index.js
@@ -1,6 +1,18 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Stats = ({good, neutral, bad}) => (
+    <>
+        <h1>Statistiikka</h1>
+        <p>Hyvä {good}</p>
+        <p>Neutraali {neutral}</p>
+        <p>Huono {bad}</p>
+        <p>Yhteensä {good + neutral + bad}</p>
+        <p>Keskiarvo {good + neutral + bad > 0 ? (good - bad) / (good + neutral + bad) : "N/A"}</p>
+        <p>Positiivisia {good + neutral + bad > 0 ? 100 * good / (good + neutral + bad) : "N/A"}</p>
+    </>
+)
+
 const App = () => {
     // tallenna napit omaan tilaansa
     const [good, setGood] = useState(0)
@@ -13,10 +25,7 @@ const App = () => {
             <button onClick={() => setGood(good + 1)}>Good</button>
             <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
             <button onClick={() => setBad(bad + 1)}>Bad</button>
-            <h1>Statistiikka</h1>
-            <p>Hyvä {good}</p>
-            <p>Neutral {neutral}</p>
-            <p>Bad {bad}</p>
+            <Stats good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
