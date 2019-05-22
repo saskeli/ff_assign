e3e84b009cc1f22a3ec58ccca4f49e424cd1f778
Dönges Saska I <saska@hpf-20.cs.helsinki.fi>
2019-05-22 10:55:45 +0300

    kurssitiedot parts 8 and 9

--- week_2/kurssitiedot/src/index.js
+++ week_2/kurssitiedot/src/index.js
@@ -28,37 +28,52 @@ const Course = ({course}) => (
 )
 
 const App = () => {
-    const course = {
-        name: 'Half Stack -sovelluskehitys',
-        parts: [
-            {
-                name: 'Reactin perusteet',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Tiedonvälitys propseilla',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'Komponenttien tila',
-                exercises: 14,
-                id: 3
-            },
-            {
-                name: "Redux",
-                exercises: 7,
-                id: 4
-            }
-        ]
-    }
-
+    const courses = [
+        {
+            name: 'Half Stack -sovelluskehitys',
+            id: 1,
+            parts: [
+                {
+                    name: 'Reactin perusteet',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Tiedonvälitys propseilla',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'Komponenttien tila',
+                    exercises: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewaret',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+  
     return (
-        <div>
-            <Course course={course} />
-        </div>
+        <>
+            <h1>Kussei</h1>
+            {courses.map((c, i) => <Course key={c.id} course={c}/>)}
+        </>
     )
-}
+  }
 
 ReactDOM.render(<App />, document.getElementById('root'));
